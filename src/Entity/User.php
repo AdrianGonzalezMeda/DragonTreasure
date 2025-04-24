08ce03de7a9493d1a8293c7b44f43a5152be2df9
Adrian Gonzalez Meda <adriangonzalezmeda@gmail.com>
2025-04-24 13:06:33 +0200

    relational embedded write with validations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -36,7 +36,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $email = null;
 
     #[ORM\Column(length: 255, unique: true)]
-    #[Groups(['user:read', 'user:write', 'treasure:item:get'])]
+    #[Groups(['user:read', 'user:write', 'treasure:item:get', 'treasure:write'])]
     #[Assert\NotBlank]
     private ?string $username = null;
 
